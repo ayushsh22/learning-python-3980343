7e7d5d6e47f15a46963e753717a0f3accc73a6a2
ayushsh22 <163711152+ayushsh22@users.noreply.github.com>
2025-04-05 03:48:25 +0000

    Start/Ch2/functions_start.py

--- Start/Ch2/functions_start.py
+++ Start/Ch2/functions_start.py
@@ -3,17 +3,42 @@
 
 
 # define a basic function
-print("hello world!")
-name = input("What is your name? ")
-print("Nice to meet you,", name)
+# def hello_func():
+#   print("hello world!")
+#   name = input("What is your name? ")
+#   print("Nice to meet you,", name)
+
+# hello_func()
 
 # function that takes parameters
+# def hello_func(greeting):
+#   print("hello world!")
+#   name = input("What is your name? ")
+#   print(greeting, name)
 
+# hello_func("how are u doing")
+# hello_func("hey whats up")
 
 # function that returns a value
+# def cube(x):
+#   return x*x*x
 
+# result = cube(3)
+# print(result)
 
 # function with default value for an parameter
+# def hello_func(greeting, name=None):
+#   print("hello world!")
+#   if name == None:
+#     name = input("What is your name? ")
+#   print(greeting, name)
 
-
+# hello_func(name="Ayush", greeting="nice to meet u")
 # function with variable number of parameters
+def multi_add(start, *args):
+  result = start
+  for x in args:
+    result = result + x
+  return result
+
+print(multi_add(0, 4, 5, 10, 4, 10))
